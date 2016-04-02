1f369a35a690254aa01e4d0576e30d6b4281fe30
Andrew <a.frailing@gmail.com>
2016-04-01 21:26:35 -0400

    Working on the REPL

--- REPL/Program.cs
+++ REPL/Program.cs
@@ -7,34 +7,24 @@ using Utility;
 
 namespace REPL {
     public class Program {
-        public enum Prompt { Commands, Repl, Help };
-        static Prompt _activePrompt;
         static string _languageName = "";
 
         public static void Main(string[] args) {
             while (true) {
-                REPLPrompt();
-                LanguagePrompt();
+                RunPrompt(Constants.ReplPromptName);
+                RunPrompt(_languageName);
             }
         }
 
-        static void REPLPrompt() {
-            RunPrompt(Constants.ReplPromptName, ConsoleKey.Escape);
-        }
-
-        static void LanguagePrompt() {
-            RunPrompt(_languageName, ConsoleKey.Escape);
-        }
-
-        static void RunPrompt(string promptName, Union<char, ConsoleKey> breakKey) {
+        static void RunPrompt(string promptName) {
             string currentText = "";
             ClearLine();
             Console.Write($"{promptName}>");
             while (true) {
                 var key = Console.ReadKey();
-                if (breakKey.Match<bool>(x => key.KeyChar == x, x => key.Key == x)) {
-                    break;
-                } else if (key.Key == ConsoleKey.Enter) {
+                if (key.Key == ConsoleKey.Escape) break;
+
+                if (key.Key == ConsoleKey.Enter) {
                     Console.Write($"\n{promptName}>");
                 } else {
                     currentText += key.KeyChar;
